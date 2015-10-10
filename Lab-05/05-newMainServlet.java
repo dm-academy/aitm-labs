7df3518f0ca4254f88cbc1c2385a6599d0b94221
Charles T. Betz <char@erp4it.com>
2015-10-10 14:47:32 -0500

    05 major

---
+++ Lab-05/05-newMainServlet.java
@@ -0,0 +1,42 @@
+GNU nano 2.2.6             File: src/main/java/biz/calavera/MainServlet.java
+
+package biz.calavera;
+
+//package test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class MainServlet extends HttpServlet {
+      // Import required java libraries
+
+        private String message;
+
+        public void init() throws ServletException
+        {
+            // Edit this message, save the file, and rebuild with Ant
+            // to see it reflected on the Web page at http://localhost:8081/MainServlet
+            message = "This is a skeleton application-- to explore the end to end Calavera delivery framework$
+        }
+
+        public void doGet(HttpServletRequest request,
+                          HttpServletResponse response)
+                  throws ServletException, IOException
+        {
+            // Set response content type
+            response.setContentType("text/html");
+
+            // Actual logic goes here.
+            PrintWriter out = response.getWriter();
+
+            Class1 oResp = new Class1(message);
+            out.println(oResp.webMessage());
+            out.println("I think seven plus six is " + oResp.sum(7,6));  // added line
+        }
+
+        public void destroy()
+        {
+            // do nothing.
+        }
+      }
