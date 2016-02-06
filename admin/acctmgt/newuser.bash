d28597231faaf3dd6223594e18d69d90cc27a342
Charles T. Betz <char@erp4it.com>
2016-02-06 09:35:42 -0600

    acctmgmt - fix for this fall

--- admin/acctmgt/newuser.bash
+++ admin/acctmgt/newuser.bash
@@ -1,7 +1,6 @@
 #!/bin/bash
 # add users in file passed on command line
-
-# 
+# need to update this to set shell to bash & related
+# see  http://askubuntu.com/questions/325807/arrow-keys-tab-complete-not-working
 
 for user in `cat $1`;do useradd -b "/home/student" -m -g student $user;done
-
